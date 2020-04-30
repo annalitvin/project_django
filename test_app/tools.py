8fc3d815177afd37424b5271ecd097d01a462234
annalitvin <litvin_any@ukr.net>
2020-04-30 19:52:40 +0300

    Updated execute_query function

--- test_app/tools.py
+++ test_app/tools.py
@@ -3,8 +3,16 @@ import os
 
 
 def execute_query(query, param=tuple()):
+
+    def dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
     db_path = os.path.join(os.getcwd(), 'chinook.db')
     conn = sqlite3.connect(db_path)
+    conn.row_factory = dict_factory
     cur = conn.cursor()
     cur.execute(query, param)
     records = cur.fetchall()
